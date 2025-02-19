725946be0f19932b93fcdcbc7d017630d78b8674
Anthony <a.stanworth97@gmail.com>
2025-02-19 12:01:31 +0000

    AS: Created level two unit test, incorporating setUp()

---
+++ unit_testing/tests_level2.py
@@ -0,0 +1,21 @@
+import unittest
+from calculator import Calculator
+
+class TestOperations(unittest.TestCase):
+    def setUp(self):
+        self.calculation = Calculator(8,2)
+        
+    def test_sm(self):
+        self.assertEqual(self.calculation.do_sum(), 10, 'The sum is wrong')
+
+    def test_minus(self):
+        self.assertEqual(self.calculation.do_minus(), 6, 'The subtraction is wrong')
+
+    def test_multiply(self):
+        self.assertEqual(self.calculation.do_multiply(), 16, 'The multiplication is wrong')
+
+    def test_divide(self):
+        self.assertEqual(self.calculation.do_divide(), 4, 'The division is wrong')
+
+if __name__ == '__main__':
+    unittest.main()
